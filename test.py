a5dc15b0e0e315b181619edc1ed31c78f6327742
123bill321 <bill70338@gmail.com>
2017-03-01 17:51:06 +0800

    fix test2

--- test.py
+++ test.py
@@ -4,15 +4,8 @@ import re
 import urllib2
 import shutil
 from bs4 import BeautifulSoup
+d = {'a':'0'}
+f = open('Textgram - write on photos APK.2.2.apk', 'r')
+d['a'] = f.read()
 
-
-res = requests.get("https://apkpure.com/kinemaster-%E2%80%93-pro-video-editor/com.nexstreaming.app.kinemasterfree")
-soup = BeautifulSoup(res.text,"html.parser")
-temps = soup.find_all('ul', {'class': 'version-ul'})
-temp = temps[0].find_all('p')
-print temp[3].text,'\n',temp[5].text#Latest Version,Publish Date:
-temps = soup.find_all('span', {'class': 'fsize'})
-temp2 = temps[0].text
-temp2 = temp2.replace('(','')
-temp2 = temp2.replace(')','')
-print temp2#Latest Version,Publish Date:
+print type(d['a'])
